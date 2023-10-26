7018bc141f75c1d48530b570bbca8348f2d790f6
Trisa Abdul Hakim <hakimpbg@gmail.com>
2023-10-26 15:54:12 +0700

    update import

--- app/Imports/AttendanceImport.php
+++ app/Imports/AttendanceImport.php
@@ -23,35 +23,40 @@ class AttendanceImport implements ToModel, WithHeadingRow, WithValidation, Skips
      * @param Collection $collection
      */
 
-    public function model(array $row){
-    //   dd($row);
+    public function model(array $row)
+    {
+        //   dd($row);
         $date = Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['date']));
         $absen_in = Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['absen_in']));
         $max_absen_in = Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['max_absen_in']));
         $status = $absen_in->gt($max_absen_in) ? 'late' : 'present';
 
-        
-        
-        $user = User::updateOrCreate(['employee_id' => $row['employee_no']], 
-        [
-            'name' => $row['employee_name'],
-            'username' => $row['employee_no'],
-            'email' => "{$row['employee_no']}@example.com",
-            'password' => bcrypt($row['employee_no']),
-        ]);
-        
+
+
+        $user = User::where('employee_id', $row['employee_no'])->first();
+        if (!$user) {
+            $user = User::create(
+                ['employee_id' => $row['employee_no']],
+                [
+                    'name' => $row['employee_name'],
+                    'username' => $row['employee_no'],
+                    'email' => "{$row['employee_no']}@example.com",
+                    'password' => bcrypt($row['employee_no']),
+                ]
+            );
+        }
+
         inputMipo($date, $absen_in, $max_absen_in, $user->id);
         return Attendance::updateOrCreate([
             'user_id' => $user->id,
             'date' => $date->format('Y-m-d'),
-        ],[
+        ], [
             'time_in' => $absen_in->format('H:i:s'),
             'max_time_in' => $max_absen_in->format('H:i:s'),
             'status' => $status,
             'time_out' => $row['absen_out'] ? Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['absen_out']))->format('H:i:s') : null,
             'description' => $row['description'] ?? '',
         ]);
-
     }
 
     public function rules(): array
@@ -78,5 +83,4 @@ class AttendanceImport implements ToModel, WithHeadingRow, WithValidation, Skips
     {
         return 1;
     }
-
 }
