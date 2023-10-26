773e3e43ad595772e184c256d1af38b87f373cce
Trisa Abdul Hakim <hakimpbg@gmail.com>
2023-10-26 14:55:26 +0700

    change footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,6 +4,6 @@
         Anything you want
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2023 <a href="https://erasoulution.co.id">Era Solution</a>.</strong> All rights
+    <strong>Copyright &copy; 2023 <a href="https://hakimm3.github.io">Trisa Abdul Hakim</a>.</strong> All rights
     reserved.
 </footer>
