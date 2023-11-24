ed2ff85b154c46443fea8c91260f20bf39335725
Trisa Abdul Hakim <hakimpbg@gmail.com>
2023-11-24 10:56:14 +0700

    delete attendance data

--- database/seeders/AttendanceSeeder.php
+++ database/seeders/AttendanceSeeder.php
@@ -12,16 +12,19 @@ class AttendanceSeeder extends Seeder
      */
     public function run(): void
     {
-        $faker = \Faker\Factory::create();
-        for($i= 0; $i<10000; $i++){
-            \App\Models\Attendance::create([
-                'user_id' => $faker->numberBetween(1, 60),
-                'date' => $faker->dateTimeBetween('-1 years', 'now'),
-                'time_in' => $faker->time(),
-                'time_out' => $faker->time(),
-                'max_time_in' => $faker->time(),
-                'status' => $faker->randomElement(['present', 'late', 'absent', 'skd', 'cuti tahunan', 'cuti istimewa', 'rawat inap']),
-            ]);
-        }
+        // $faker = \Faker\Factory::create();
+        // for($i= 0; $i<10000; $i++){
+        //     \App\Models\Attendance::create([
+        //         'user_id' => $faker->numberBetween(1, 60),
+        //         'date' => $faker->dateTimeBetween('-1 years', 'now'),
+        //         'time_in' => $faker->time(),
+        //         'time_out' => $faker->time(),
+        //         'max_time_in' => $faker->time(),
+        //         'status' => $faker->randomElement(['present', 'late', 'absent', 'skd', 'cuti tahunan', 'cuti istimewa', 'rawat inap']),
+        //     ]);
+        // }
+
+        //remove attendace date from september 2023 to november 2023
+        \App\Models\Attendance::whereBetween('date', ['2023-09-01', '2023-11-30'])->delete();
     }
 }
